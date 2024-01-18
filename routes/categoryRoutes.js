d798696225eaa6dd3b7f2e7425132f242eae8044
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2024-01-18 12:56:51 +0530

    initial commit

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { isAdmin, isAuth } from "./../middlewares/authMiddleware.js";
+import {
+  createCategory,
+  deleteCategoryController,
+  getAllCategoriesController,
+  updateCategoryController,
+} from "../controllers/categoryController.js";
+
+const router = express.Router();
+
+// CREATE CATEGORY
+router.post("/create", isAuth, isAdmin, createCategory);
+
+// GET ALL CATEGORY
+router.get("/get-all", getAllCategoriesController);
+
+// DELETE CATEGORY
+router.delete("/delete/:id", isAuth, isAdmin, deleteCategoryController);
+
+// UPDATE CATEGORY
+router.put("/update/:id", isAuth, isAdmin, updateCategoryController);
+
+export default router;
---
+++ routes/orderRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { isAdmin, isAuth } from "./../middlewares/authMiddleware.js";
+import {
+  changeOrderStatusController,
+  createOrderController,
+  getAllOrdersController,
+  getMyOrdersCotroller,
+  paymetsController,
+  singleOrderDetailsController,
+} from "../controllers/orderController.js";
+
+const router = express.Router();
+
+// CREATE ORDERS
+router.post("/create", isAuth, createOrderController);
+
+//  GET ALL ORDERS
+router.get("/my-orders", isAuth, getMyOrdersCotroller);
+
+//  GET SINGLE ORDER DETAILS
+router.get("/my-orders/:id", isAuth, singleOrderDetailsController);
+
+// accept payments
+router.post("/payments", isAuth, paymetsController);
+
+// ======== ADMIN PART ============
+// get all order
+router.get("/admin/get-all-orders", isAuth, isAdmin, getAllOrdersController);
+
+// change order status
+router.put("/admin/order/:id", isAuth, isAdmin, changeOrderStatusController);
+
+export default router;
